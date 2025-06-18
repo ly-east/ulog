41e859fa85d6ca7d0238072926487d8adc5e6299
shin <ozziewei@gmail.com>
2025-06-18 12:53:33 +0800

    [resource]Add version and description files

---
+++ resource/version/Version.h
@@ -0,0 +1,11 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// clang-format off
+#define ULOG_VERSION_MAJOR 0
+#define ULOG_VERSION_MINOR 5
+#define ULOG_VERSION_PATCH 0
+#define ULOG_VERSION       "0.5.0"
+// clang-format on
+
+#endif // VERSION_H
